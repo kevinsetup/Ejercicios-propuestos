622a71bfcf15062fcf21b401434bd1545f1c957d
keu <kevinsetup900@gmail.com>
2020-05-12 16:14:18 -0500

    Actualizacion2

--- TareaLogin/js/script.js
+++ TareaLogin/js/script.js
@@ -13,28 +13,33 @@ contraseña(){
 return this.contraseña;
 };
 };
-
+var intentos = 0;
 function validar(){
-    var intentos = 3;
+    
 
 var user = new Usuario(document.getElementById('username').value, 
 document.getElementById('password').value
 );
 var usernametext = "kevin"
 var passwordtext = "123";
-if(intentos == 3){
-if(user.nombre == usernametext && user.contraseña == passwordtext){
-    
+
+
+if(intentos != 3){
+if(user.nombre == usernametext && user.contraseña == passwordtext){   
 alert("Bienvenido");
 }else{
-    alert("usuario o contraseña incorrecta");
-   intentos--;
+ alert("usuario o contraseña incorrecta");
+ intentos++;
   alert(intentos);
 
 
+}
+if(intentos == 3){
+document.write("Hemos bloqueado tu dirección ip a esta página por 3 intentos fallidos al administrador");
+
+}
 }
 
    
 
 };
-}
